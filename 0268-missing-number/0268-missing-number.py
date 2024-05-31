0e46a26ea837cf1f3350b977417e6e1600d81796
harshareddy07 <56046182+harshareddy07@users.noreply.github.com>
2024-05-31 20:46:23 +0530

    Time: 85 ms (94.75%), Space: 12.8 MB (93.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -11,8 +11,15 @@ class Solution(object):
        #     if i not in nums:
         #        return i
 
-        n = len(nums)
-        expectedSum = (n*(n+1))//2
-        actualSum = sum(nums)
-        return expectedSum-actualSum
+       # n = len(nums)
+        #expectedSum = (n*(n+1))//2
+        #actualSum = sum(nums)
+        #return expectedSum-actualSum
+
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += (i-nums[i])
+
+        return res
         
