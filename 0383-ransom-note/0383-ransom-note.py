b22af2731c2b9c8945a794efefed2c420640c566
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-10-13 18:32:52 +0200

    Time: 16 ms (62.21%), Space: 17.8 MB (82.26%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,16 +1,10 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        countMag = {}
-
-        for c in magazine:
-            countMag[c] = countMag.get(c, 0) + 1
-        
-        for t in ransomNote:
-            if t not in countMag:
+        ransom = Counter(ransomNote)
+        mag = Counter(magazine)
+       
+        for key, val in ransom.items():
+            if key not in mag or mag[key] < val:
                 return False
-            elif countMag[t] == 1:
-                del countMag[t]
-            else:
-                countMag[t] -= 1
-
         return True
