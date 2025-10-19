10c5cb219d060ad6e4966050c53d8119331fd976
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-10-19 17:35:21 +0200

    Update 0234-palindrome-linked-list.py

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -32,3 +32,30 @@ class Solution(object):
             right = right.next
         
         return True
+
+
+**Solution - 2 with array ( O(n) )**
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+
+        l, r = 0, len(nums) - 1
+        while l <= r :
+            if nums[l] != nums[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
