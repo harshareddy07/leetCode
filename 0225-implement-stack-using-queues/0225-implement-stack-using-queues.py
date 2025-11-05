fd834de5b5e0ffc13a92337ac7d273a22f58555c
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-11-05 23:52:15 +0100

    Time: 0 ms (100%), Space: 17.8 MB (90.21%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,35 +1,26 @@
 class MyStack:
 
     def __init__(self):
-        self.q1 = deque()
-        self.q2 = deque()
+        self.q = deque()
 
-    def push(self, x) :
-        self.q1.append(x)
+    def push(self, x: int) -> None:
+        self.q.append(x)
 
-    def pop(self):
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
-        
-        popped_element = self.q1.popleft()
-        
-        # Swap q1 and q2
-        self.q1, self.q2 = self.q2, self.q1
-        
-        return popped_element
+    def pop(self) -> int:
+        for i in range(len(self.q)-1):
+            self.push(self.q.popleft())
+        return self.q.popleft()
 
-    def top(self) :
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
-        
-        top_element = self.q1[0]
-        
-        self.q2.append(self.q1.popleft())
-        
-        # Swap q1 and q2
-        self.q1, self.q2 = self.q2, self.q1
-        
-        return top_element
+    def top(self) -> int:
+        return self.q[-1]
 
-    def empty(self) :
-        return len(self.q1) == 0
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
