13eadd248632ca61c07f85b2d2c58871d4e5511c
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-12-04 22:11:54 +0530

    Time: 111 ms (66.67%), Space: 38.2 MB (5.58%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,22 +1,45 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        # approach - 1
+        # l = 0
+        # curr_sum = 0
+        # count = defaultdict(int)
+        # res = 0
+
+        # for r in range(len(nums)):
+        #     count[nums[r]] += 1
+        #     curr_sum += nums[r]
+
+        #     if r- l + 1 > k:
+        #         count[nums[l]] -= 1
+        #         if count[nums[l]] == 0:
+        #             count.pop(nums[l])
+        #         curr_sum -= nums[l]
+        #         l+= 1
+
+        #     if len(count) == r - l + 1 ==k:
+        #         res = max(curr_sum, res)
+
+        # return res
+
+
+        # approach - 2
         l = 0
-        curr_sum = 0
-        count = defaultdict(int)
+        prev_index = {}
         res = 0
+        curr_sum = 0
 
         for r in range(len(nums)):
-            count[nums[r]] += 1
             curr_sum += nums[r]
+            i = prev_index.get(nums[r], -1)
 
-            if r- l + 1 > k:
-                count[nums[l]] -= 1
-                if count[nums[l]] == 0:
-                    count.pop(nums[l])
+            while l <= i or r- l + 1 > k:
                 curr_sum -= nums[l]
                 l+= 1
 
-            if len(count) == r - l + 1 ==k:
+            if r - l + 1 ==k:
                 res = max(curr_sum, res)
+
+            prev_index[nums[r]] = r
 
         return res
