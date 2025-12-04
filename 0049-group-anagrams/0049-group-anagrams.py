349d97fe7ab55d5f34faccd9eb6f0eb118c1b044
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-12-04 22:28:26 +0530

    Time: 21 ms (22.94%), Space: 22.8 MB (11.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,30 +1,14 @@
-# (1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0) for eat , ate
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        res = defaultdict(list)
-        for s in strs:
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result =  defaultdict(list)
+
+        for chars in strs:
             count = [0] * 26
-            #print(count)
-            for c in s:
+            for c in chars:
                 count[ord(c) - ord("a")] += 1
-                
-            #print("c2", tuple(count))
+            
+            result[tuple(count)].append(chars)
 
-            res[tuple(count)].append(s)
+        return list(result.values()) 
 
-        return res.values()
-
-**method - 2**
-If you don’t want to use defaultdict, you can use dict + setdefault:
-
-res = {}
-for s in strs:
-    count = [0] * 26
-    for c in s:
-        count[ord(c) - ord("a")] += 1
-    key = tuple(count)
-    res.setdefault(key, []).append(s)
+        
