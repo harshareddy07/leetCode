34ee2c9bea8fcd14186caef0e3c6ccc4ba3ed54e
Harshavardhan <56046182+harshareddy07@users.noreply.github.com>
2025-12-06 00:45:35 +0530

    Time: 47 ms (73.87%), Space: 34.3 MB (30.28%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,40 +1,17 @@
-# first check each value has begining number(-1) if its not there then its a start value
-# if doesnt have right values then it doesnt have any sequence
-# if does have right value then keep on check the sequence(ex: 1->2->3->4)
-
-class Solution(object):
-    def longestConsecutive(self, nums):
-        
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return 0 
         nums=list(set(nums))
         nums.sort()
-        m=0
-        c=1
-        for i in range(1,len(nums)):
-            # print(nums)
-            # print("i", nums[i]-nums[i-1])
-            if nums[i]-nums[i-1]==1:
-                c+=1
-                # print(c)
+        res = 0
+        curr = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i-1] == 1:
+                curr += 1
             else:
-                # print("mc", m , c)
-                m=max(m,c)
-                c=1
-        return max(m,c)
+                res = max(curr, res)
+                curr = 1
 
-        # Method - 2
-        # numset = set(nums)
-        # longest_value = 0
-
-        # for n in nums:
-        #     if (n-1) not in numset:
-        #         length = 0
-        #         while (n+length) in numset:
-        #             length += 1
-        #         longest_value = max(length, longest_value)
-        
-        # return longest_value
-
-
-
+        return max(curr, res)
